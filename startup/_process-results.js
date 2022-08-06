d4757bba6c5608b88df2a1ca4d37ee7ffbf81905
MP <markpaololibunao@gmail.com>
2022-08-06 20:43:31 +0800

    Commits updated script that sorts by cold start

--- startup/_process-results.js
+++ startup/_process-results.js
@@ -33,27 +33,42 @@ let md = `
 | | startup(ms) | listen(ms) |
 |-| -       | -      |`;
 
-for (const r of results) {
-  const data = fs.readFileSync(path.join(__dirname, r), { encoding: "utf-8" });
-  const lines = data.split("\n").filter(Boolean);
-  const temp = {
-    startup: 0,
-    listen: 0,
-  };
-  lines.forEach((x) => {
-    const [startup, listen] = x.split("|");
-    temp.startup += readableHRTimeMs(
-      startup.split(",").map((x) => parseInt(x)),
-    );
-    temp.listen += readableHRTimeMs(listen.split(",").map((x) => parseInt(x)));
+results
+  .map((result) => {
+    const data = fs.readFileSync(path.join(__dirname, result), {
+      encoding: "utf-8",
+    });
+    const lines = data.split("\n").filter(Boolean);
+    const temp = {
+      startup: 0,
+      listen: 0,
+    };
+    lines.forEach((x) => {
+      const [startup, listen] = x.split("|");
+      temp.startup += readableHRTimeMs(
+        startup.split(",").map((x) => parseInt(x)),
+      );
+      temp.listen += readableHRTimeMs(
+        listen.split(",").map((x) => parseInt(x)),
+      );
+    });
+
+    return {
+      name: result,
+      startup: (temp.startup / lines.length).toFixed(2),
+      listen: (temp.listen / lines.length).toFixed(2),
+    };
+  })
+  .sort((a, b) => {
+    if (a.listen > b.listen) return 1;
+    if (a.listen < b.listen) return -1;
+    return 0;
+  })
+  .forEach((s) => {
+    md += `\n| ${s.name.replace(".txt", "")} | ${s.startup} | ${s.listen} |`;
   });
-  md += `\n| ${r.replace(".txt", "")} | ${(temp.startup / lines.length).toFixed(
-    2,
-  )} | ${(temp.listen / lines.length).toFixed(2)} |`;
-}
 
 if (process.argv.length >= 3 && process.argv[2] === "-u") {
   console.debug("Updating METRICS...");
   updateReadme(md);
 }
-console.log(md);
